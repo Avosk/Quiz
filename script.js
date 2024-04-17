1c81e4e8808dc190f3db5cab5cc3dac58ea0ce63
Avosk <120457217+Avosk@users.noreply.github.com>
2024-04-17 12:42:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let question_field = document.querySelector('.question')
 let answer_buttons = document.querySelectorAll('.answer')
 let container_h3 = document.querySelector('.container_h3')
-let start_button = document.querySelector('.start')
+let start_button = document.querySelector('.start-btn')
 let container_main  = document.querySelector('.main')
+let container_start = document.querySelector('.start')
+
 function randit(min, max){
     return Math.round(Math.random()*(max-min)+min)
 }
@@ -18,6 +20,19 @@ function shuffle(array){
         [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
     return array;
+}
+let cookie = false
+let cookies = document.cookie.split('; ')
+for (let i = 0; i < cookies.length; i += 1) {
+    if (cookies[i].split('=')[0] == 'numbers_high_score') {
+        cookie = cookies[i].split('=')[1]
+        break
+    }
+}
+
+if (cookie) {
+    let data = cookie.split('/')
+    container_start_h3.innerHTML = `<h3>В прошлый раз вы дали ${data[1]} правильных ответов из ${data[0]}. Точность - ${Math.round(data[1] * 100 / data[0])}%.</h3>`
 }
 class Question{
     constructor(){
@@ -58,15 +73,17 @@ let current_question
 // current_question.display()
 start_button.addEventListener('click', function(){
     container_main.style.display='flex'
-    start_button.style.display = 'none'
+    container_start.style.display = 'none'
     total_answer_given = 0
     correct_answer_given =0
     current_question = new Question()
 current_question.display()
 
 setTimeout(function(){
+    let new_cookie = `numbers_high_score=${total_answers_given}/${correct_answers_given}; max-age=10000000000`
+    document.cookie = new_cookie
     container_main.style.display ='none'
-    start_button.style.display = 'flex'
+    container_start.style.display = 'flex'
     container_h3.innerHTML= `Вы далм ${correct_answer_given} правильных ответов из ${total_answer_given},
     Точность - ${Math.round(correct_answer_given*100/total_answer_given)}%.`
 }, 10000)})
